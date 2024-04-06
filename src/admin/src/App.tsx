bd8d6443f1be74c3d75d921e6d3749334958093a
Revazashvili <levanrevazashvili@gmail.com>
2024-04-06 17:42:51 +0400

    make TranslationCompoment modal

--- src/admin/src/App.tsx
+++ src/admin/src/App.tsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Button } from "antd";
+import {useState} from "react";
 import TranslationComponent from "./components/TranslationComponent.tsx";
-import {Translation} from "../../shared/DictionaryModels.ts";
+import {Language, Translation} from "../../shared/DictionaryModels.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-    function onSave(id: number, nameTranslations: Translation[]) {
+    const [modalIsOpen, setModalOpen] = useState(false);
+
+    function getId() {
+        return 1;
+    }
+
+    function onSave(id: number, nameTranslations: Translation[]){
         console.log(id);
         console.log(nameTranslations);
+        setModalOpen(false);
     }
 
-    function getId(): number{
-      return 1;
-    }
-
-  return (
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        <TranslationComponent id={getId()} nameTranslations={[]} onSave={onSave}/>
+        <Button type="primary" onClick={() => setModalOpen(true)}>
+            Vertically centered modal dialog
+        </Button>
+
+        <TranslationComponent
+            id={getId()}
+            nameTranslations={[new Translation(Language.Ka,"ქართული"), new Translation(Language.En,"English")]}
+            title={"თემა"}
+            isOpen={modalIsOpen}
+            onSave={onSave}
+            onCancel={() => setModalOpen(false)}
+        />
     </>
   )
 }
--- src/admin/src/components/TranslationComponent.tsx
+++ src/admin/src/components/TranslationComponent.tsx
@@ -1,53 +1,64 @@
-import React, { useState } from 'react';
-import {Language, Translation} from "../../../shared/DictionaryModels.ts";
+import React, { useState } from "react";
+import { Language, Translation } from "../../../shared/DictionaryModels.ts";
+import { Input, Modal } from "antd";
 
 interface Props {
-    id: number;
-    nameTranslations: Translation[];
-    onSave: (id: number, nameTranslations: Translation[]) => void;
+  id: number;
+  nameTranslations: Translation[];
+  title: string;
+  isOpen: boolean;
+  onSave: (id: number, nameTranslations: Translation[]) => void;
+  onCancel: () => void;
 }
 
 const TranslationComponent = (props: Props) => {
-    const [georgianText, setGeorgianText] = useState('');
-    const [englishText, setEnglishText] = useState('');
+  const [georgianText, setGeorgianText] = useState(props.nameTranslations.find(t => t.Language == Language.Ka)?.Value ?? '');
+  const [englishText, setEnglishText] = useState(props.nameTranslations.find(t => t.Language == Language.En)?.Value ?? '');
 
-    const handleGeorgianInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setGeorgianText(event.target.value);
-    };
+  const handleGeorgianInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setGeorgianText(event.target.value);
+  };
 
-    const handleEnglishInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEnglishText(event.target.value);
-    };
+  const handleEnglishInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEnglishText(event.target.value);
+  };
 
-    return (
-        <>
-            <div>
-                <div>
-                    <label htmlFor="georgianInput">Enter Georgian Translation: </label>
-                    <input
-                        type="text"
-                        id="georgianInput"
-                        value={georgianText}
-                        onChange={handleGeorgianInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="englishInput">Enter English Translation: </label>
-                    <input
-                        type="text"
-                        id="englishInput"
-                        value={englishText}
-                        onChange={handleEnglishInputChange}
-                    />
-                </div>
-                <div>
-                    <button onClick={() => {
-                        props.onSave(props.id, [new Translation(Language.Ka,georgianText),new Translation(Language.En,englishText)]);
-                    }}>Save</button>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Modal
+        title={props.title}
+        centered
+        open={props.isOpen}
+        cancelText="გაუქმება"
+        okText="შენახვა"
+        onOk={() => {
+          const nameTranslations = [
+            new Translation(Language.Ka, georgianText),
+            new Translation(Language.En, englishText),
+          ];
+          props.onSave(props.id, nameTranslations);
+        }}
+        onCancel={props.onCancel}
+      >
+        <Input
+          placeholder="ქართულად"
+          value={georgianText}
+          onChange={handleGeorgianInputChange}
+        />
+        <br />
+        <br />
+        <Input
+          placeholder="ინგლისურად"
+          value={englishText}
+          onChange={handleEnglishInputChange}
+        />
+      </Modal>
+    </>
+  );
 };
 
 export default TranslationComponent;
